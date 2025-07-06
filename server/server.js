cdd4c4d1c0f19e4d4c9e5c8bd33f32e58e92d323
neha-bytedev <neha.saman567@gmail.com>
2025-07-06 18:13:45 +0530

    #

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './configs/mongodb.js'
+
+//Initialize Express
+const app = express()
+
+// Connect to database
+await connectDB()
+
+//Middlewares
+app.use( cors())
+
+//Routes
+app.get('/',(req,res)=> res.send("API Working"))
+
+//PORT 
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT , ()=>{
+  console.log(`Server is running on PORT ${PORT}`)
+})
