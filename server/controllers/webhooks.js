39a6dc9cc9298be1ff4e937aaeed6f146e021654
neha-bytedev <neha.saman567@gmail.com>
2025-07-07 18:26:36 +0530

    #

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -17,16 +17,20 @@ whook.verify(req.body, {
 // Step 2: Parse raw body to JSON
 const { data, type } = JSON.parse(req.body.toString("utf8"));
 
+ 
    
    switch (type) {
-    case 'user.created':{
+    case "user.created":{
       const userData = {
         _id: data.id,
         email: data.email_addresses[0].email_address,
         name: data.first_name + " " + data.last_name,
         imageUrl: data.image_url,
-      }
-      await User.create(userData)
+      };
+     
+
+      await User.create(userData);
+      
       res.json({})
       break;
     }
@@ -52,6 +56,8 @@ const { data, type } = JSON.parse(req.body.toString("utf8"));
    }
 
   } catch (error) {
-     res.json({success: false, message: error.message})
+    
+    return res.status(400).json({ success: false, message: error.message });
+     
   }
 }
