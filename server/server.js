b9c1a906a0b798ab471994a1cf841032befb1606
neha-bytedev <neha.saman567@gmail.com>
2025-07-27 17:29:48 +0530

    #

--- server/server.js
+++ server/server.js
@@ -5,12 +5,15 @@ import connectDB from './configs/mongodb.js'
 import { clerkWebhooks } from './controllers/webhooks.js'
 import educatorRouter from './routes/educatorRoutes.js'
 import { clerkMiddleware } from '@clerk/express'
+import connectCloudinary from './configs/cloudinary.js'
+import courseRouter from './routes/courseRoute.js'
 
 //Initialize Express 
 const app = express()
 
 //Connect to database
 await connectDB()
+await connectCloudinary()
 
 //Middlewares
 app.use(cors())
@@ -18,8 +21,9 @@ app.use(clerkMiddleware())
 
 //Routes
 app.get ('/',(req,res)=> res.send("API Working"))
-
+app.post('/clerk', express.json(),clerkWebhooks)
 app.use('/api/educator',express.json(),educatorRouter)
+app.use('/api/courses', express.json(),courseRouter)
 
 
 
